eaa21cec7abb94deef57375551d9c4f028bfd545
unknown <azeezumarfaruk@gmail.com>
2022-09-17 01:33:03 +0100

    feat:Added fontFamily objects in the theme object

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       fontFamily: {
         Montserrat: ["Montserrat", "sans-serif"],
-        Roboto: ["Roboto", "sans-serif"],
+        RobotoCondensed: ["Roboto Condensed", "sans-serif"],
       },
     },
   },
