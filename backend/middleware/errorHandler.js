2ab57677465b7c746a8cfe2307ba680a04d6dcb8
unknown <azeezumarfaruk@gmail.com>
2022-10-03 12:47:27 +0100

    [fix] : modified response for err instance of custom error

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,10 +1,11 @@
 const CustomErrorHandler = require("../errors/customErrorHandler");
 
 const errorHandler = (err, req, res, next) => {
-  if (err instanceof CustomErrorHandler) {
-    res.status(CustomErrorHandler.statusCode).json({ message: CustomErrorHandler.message });
-  }
-  res.status(500).json({ message: "Something went wrong" });
+	console.log(err.message);
+	if (err instanceof CustomErrorHandler) {
+		res.status(err.statusCode).json({ message: err.message });
+	}
+	res.status(500).json({ message: "Something went wrong" });
 };
 
 module.exports = errorHandler;
