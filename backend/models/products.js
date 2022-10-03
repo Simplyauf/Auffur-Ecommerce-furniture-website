dcd62e1259549d66266e32c5c7baed98f39024de
unknown <azeezumarfaruk@gmail.com>
2022-10-03 12:52:04 +0100

    [feat] : modified the product schemas

--- backend/models/products.js
+++ backend/models/products.js
@@ -1,19 +1,49 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Product's title is required"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Product's price is required"],
+    },
+    discountPercentValue: {
+      type: Number,
+      default: function () {
+        return !this.discountPercentValue && 0;
+      },
+    },
+    image: {
+      type: String,
+      required: [true, "Product's image is required"],
+    },
+    categories: {
+      type: [String],
+      enums: [
+        "chairs",
+        "tables",
+        "kids",
+        "office",
+        "sets",
+        "cupboards",
+        "lighting",
+        "discounts",
+        "bedroom",
+        "living room",
+        "kitchen",
+        "dining",
+        "featured",
+        "first order deal",
+      ],
+      required: [true, "Product's categories is required"],
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  categories: {
-    type: String,
-    enums: ["chairs", "tables", "kids", "lightning"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Product", productSchema);
