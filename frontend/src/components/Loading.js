f33c3632c3015b98249301fddbb5d279a65278e3
unknown <azeezumarfaruk@gmail.com>
2022-10-08 17:42:46 +0100

    [feat] : added new files to component folder

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,16 @@
+export const Loading = () => {
+  return (
+    <div className="w-[100%] h-[50vh]">
+      <div className="flex flex-col items-center flex-wrap gap-[24px] justify-center w-[100%] h-[100%]">
+        <div className="flex justify-center items-center  text-[25px] tablet:text-[30px] md:text-[32px] ">
+          <h2 className="mr-4 font-NunitoSans font-extrabold">Loading</h2>
+          <div className="flex items-center gap-4 ">
+            <span className="preloading-dot "></span>
+            <span className="preloading-dot"></span>
+            <span className="preloading-dot"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/components/wishlistSection.js
@@ -0,0 +1,1 @@
+wishlistSection.js
