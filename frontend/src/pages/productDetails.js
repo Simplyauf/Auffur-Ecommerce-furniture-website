44bc6d1ecea9b49f079560c6de5675fbc1ec4ef3
unknown <azeezumarfaruk@gmail.com>
2022-12-09 11:26:06 +0100

    [feat] : Added functionalities for adding to wishlist,showing states of product,whether it is in cart/wishlist or not,whether a product is out of stock or not

--- frontend/src/pages/productDetails.js
+++ frontend/src/pages/productDetails.js
@@ -3,18 +3,23 @@ import { useNavigate, useParams } from "react-router-dom";
 import { FiHeart } from "react-icons/fi";
 import FooterSection from "../components/footerSection";
 import { useSelector, useDispatch } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { handleCartModification } from "../utils/handleCartModification";
+import { handleWishlistModification } from "../utils/handleWishlistModification";
+import { isProductInCartFn, isProductInWishlistFn } from "../utils/isSpecificProductInCartAndWishlist.js";
 
 export const ProductDetailsPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { allProductsData } = useSelector((state) => state.productsData);
+  const { wishlist, cart } = useSelector((state) => state.wishlistAndCartSection);
   const [productQuantityInCart, setProductQuantityInCart] = useState(1);
+  const [isWishlisted, setIsWishlisted] = useState(false);
+  const [isProductInCart, setIsProductInCart] = useState(false);
 
   const { productId } = useParams();
   const currentProduct = allProductsData.find((product) => product._id === productId);
-  const { _id, title, price, image, discountPercentValue, categories } = currentProduct;
+  const { _id, title, price, image, discountPercentValue, categories, stock } = currentProduct;
 
   //loop through and get the sub categories arr
   let subCategoriesArr = [];
@@ -28,6 +33,15 @@ export const ProductDetailsPage = () => {
     }
     handleCartModification(_id, dispatch, productQuantityInCart);
   };
+
+  // Checks if a the current product can be found in the wishlist and cart so as to be able to display the states in the ui
+  useEffect(() => {
+    isProductInWishlistFn(_id, setIsWishlisted, wishlist);
+  }, [wishlist]);
+
+  useEffect(() => {
+    isProductInCartFn(_id, setIsProductInCart, cart);
+  }, [cart]);
 
   let discountedPrice = price - (price * discountPercentValue) / 100;
   return (
@@ -50,9 +64,14 @@ export const ProductDetailsPage = () => {
         <div className="w-[100%] h-[320px] mx-auto bg-[#e5e5e5] relative flex justify-center items-center">
           <img src={image} alt="" className="w-[100%] max-w-[95%] h-auto max-h-[95%] object-cover" />
           <div
-            className={`absolute p-3 bg-[#ffffff] shadow-[0px_2px_8px_0px_#00000085] rounded-[50%] top-[5%] right-[5%] z-[100]`}
+            className={`absolute p-3 bg-[#ffffff] shadow-[0px_2px_8px_0px_#00000085] rounded-[50%] top-[5%] right-[5%] z-[100] ${
+              isWishlisted && "bg-[#fca311]"
+            }`}
           >
-            <FiHeart className={`w-6 h-6`} />
+            <FiHeart
+              className={`w-6 h-6 ${isWishlisted && "fill-[#fca311] stroke-white"}`}
+              onClick={() => handleWishlistModification(_id, dispatch)}
+            />
           </div>
         </div>
         <h2 className="text-[24px] font-bold tracking-[0.5px]">{title}</h2>
@@ -64,11 +83,11 @@ export const ProductDetailsPage = () => {
             </h3>
           </div>
         ) : (
-          <h3 className="font-bold text-[20px] tracking-[1px]">${price.toFixed(2)}</h3>
+          <h3 className="font-bold text-[20px] tracki2ng-[1px]">${price.toFixed(2)}</h3>
         )}
         <div className="flex gap-1">
           <h3 className="font-bold tracking-[0.5px] text-[18px]">Availability :</h3>
-          <span> Out of stock</span>
+          <span>{stock > 0 ? "In stock" : " Out of stock"}</span>
         </div>
         <div>
           <h2 className="font-bold text-[18px] tracking-[0.5px]">Description</h2>
@@ -97,7 +116,7 @@ export const ProductDetailsPage = () => {
           className="text-[#14213d] bg-transparent border-[1px] border-[#14213d] font-semibold w-[100%] h-[50px]"
           onClick={handleAddToCartFn}
         >
-          Add to Cart
+          {isProductInCart ? "Remove from Cart" : "Add to Cart"}
         </button>
         <button className="text-white bg-[#fca311] font-semibold w-[100%] h-[50px]">Buy Now</button>
         <div className="flex-col flex gap-4">
