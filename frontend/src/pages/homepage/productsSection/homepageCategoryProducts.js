e1c85143d1bb2f2cd7dde7ad200600bd37177ad0
Auf <azeezumarfaruk@gmail.com>
2022-12-11 23:35:16 +0100

    [fix & feat] : fix the site crash caused by not mapping data as prop into 'singleProductBox',added functionalities for displayin categories title and products

---
+++ frontend/src/pages/homepage/productsSection/homepageCategoryProducts.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BsEye } from "react-icons/bs";
+import { FiHeart } from "react-icons/fi";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { SingleProductBox } from "../../../components/singleProductBox";
+
+export const HomepageCategoryProducts = ({ currentlyRequestedCategories }) => {
+  return (
+    <div className="w-[80%] mx-auto items-center justify-center gap-10  flex flex-col">
+      {currentlyRequestedCategories.map((productData) => (
+        <SingleProductBox productsData={productData} key={productData._id} />
+      ))}
+      <div className="self-end flex gap-3 items-center ">
+        <button className="text-[18px] text-[#fca311] font-semibold hover:underline transition underline-offset-[6px] decoration-2">
+          Shop for more
+        </button>
+        <FaLongArrowAltRight className="w-6 h-6 fill-[#fca311]" />
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/homepage/productsSection/index.js
+++ frontend/src/pages/homepage/productsSection/index.js
@@ -1,26 +1,94 @@
 import { DealOfTheMonth } from "./dealOfTheMonth";
-import { ProductCategories } from "./productCategories";
+import { HomepageCategoryProducts } from "./homepageCategoryProducts";
 import { FeaturedCategories } from "./featuredCategories";
+import { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Index = () => {
+  const [currentlyRequestedCategories, setCurrentlyRequestedCategories] =
+    useState([]);
+  const categoryContainerRef = useRef();
+  const { allProductsData } = useSelector((state) => state.productsData);
+
+  useEffect(() => {
+    shuffleArr(featuredProducts);
+  }, [allProductsData]);
+
+  const shuffleArr = (Arr) => {
+    let slicedShuffledArr = Arr.sort(() => Math.random() - 0.5).slice(0, 10);
+    setCurrentlyRequestedCategories(slicedShuffledArr);
+  };
+
+  const featuredProducts = allProductsData.filter((products) =>
+    products.categories["Featured Categories"].includes("featured")
+  );
+  const firstOrderDeals = allProductsData.filter((products) =>
+    products.categories["Featured Categories"].includes("first order deal")
+  );
+  const bestDeals = allProductsData.filter((products) =>
+    products.categories["Featured Categories"].includes("discounts")
+  );
+
+  const categoriesArr = {
+    featuredProducts: featuredProducts,
+    firstOrderDeals: firstOrderDeals,
+    bestDeals: bestDeals,
+  };
+
+  const handleCategoryClick = (e) => {
+    for (let key in categoriesArr) {
+      if (e.target.dataset.id === key) {
+        e.target.parentElement.classList.add("homepage-active-category-tab");
+        e.target.parentElement.style.order = 1;
+        shuffleArr(categoriesArr[key]);
+
+        // order 1 is assigned to the default categoryDom and the other unclicked categoryDom gets order style values from the orderNumbering variable
+        let orderNumbering = [0, 2];
+        let categoriesDomListArr = Array.from(
+          categoryContainerRef.current.children
+        );
+        let theNonTargetedCategoryDomArr = categoriesDomListArr.filter(
+          (elem) => elem !== e.target.parentElement
+        );
+        theNonTargetedCategoryDomArr.map((elem, i) => {
+          elem.classList.remove("homepage-active-category-tab");
+          elem.style.order = orderNumbering[i];
+          return null;
+        });
+      }
+    }
+  };
+
   return (
     <>
       <h1 className="text-[36px] text-center font-bold">Our shop</h1>
-      <div className="flex text-[20px] justify-center items-center gap-6 my-8">
-        <div>
-          <h2 className="text-center ">Trending </h2>
-          <div className="bg-[#fca311] h-[3px] w-[100%]"></div>
+      <div
+        className="flex text-[20px] whitespace-nowrap items-center  mx-auto w-[80%] overflow-scroll flex-nowrap gap-6 my-8"
+        onClick={(e) => handleCategoryClick(e)}
+        ref={categoryContainerRef}
+      >
+        <div className="cursor-pointer order-1 transition-all ease-in-out duration-[0.25s] homepage-active-category-tab">
+          <h2 data-id="featuredProducts" className="text-center ">
+            Featured{" "}
+          </h2>
+          <div className="bg-[#fca311] h-[3px] w-0 "></div>
         </div>
-        <div>
-          <h2 className="text-center ">New arrivals</h2>
+        <div className="cursor-pointer order-2 transition-all ease-in-out duration-[0.25s]">
+          <h2 data-id="firstOrderDeals" className="text-center ">
+            First order deals
+          </h2>
           <div className="bg-[#fca311] h-[3px] w-0"></div>
         </div>
-        <div>
-          <h2 className="text-center ">Featured</h2>
+        <div className="cursor-pointer order-0 transition-all ease-in-out duration-[0.25s]">
+          <h2 data-id="bestDeals" className="text-center">
+            Best Deals
+          </h2>
           <div className="bg-[#fca311] h-[3px] w-0"></div>
         </div>
       </div>
-      <ProductCategories />
+      <HomepageCategoryProducts
+        currentlyRequestedCategories={currentlyRequestedCategories}
+      />
       <FeaturedCategories />
       <DealOfTheMonth />
     </>
