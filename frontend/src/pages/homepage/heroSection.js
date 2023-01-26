dd7a750c7bca3d14779d45c2ac2073f98eab2ca8
Auf <azeezumarfaruk@gmail.com>
2023-01-26 21:42:25 +0100

    [style] : made the bg img of hero section and also category section responsive

--- frontend/src/pages/homepage/heroSection.js
+++ frontend/src/pages/homepage/heroSection.js
@@ -1,9 +1,14 @@
 import React from "react";
+import heroImg from "../../assets/heroBg.jpg";
 
 export const HeroSection = () => {
+  console.log(heroImg);
   return (
     <>
-      <div className="w-[100%] h-[400px] md:h-[450px] bg-[url('https://images.pexels.com/photos/2227832/pexels-photo-2227832.jpeg?auto=compress&cs=tinysrgb&w=600')] bg-cover bg-no-repeat bg-center top-0 relative">
+      <div
+        style={{ backgroundImage: `url(${heroImg})` }}
+        className="w-[100%] h-[300px] tablet:h-[400px] md:h-[452px] lg:h-[560px] xl:h-[652px] 2xl:h-[800px]  bg-cover bg-no-repeat bg-center top-0 relative"
+      >
         <div className="overlay absolute top-0 left-0 right-0 w-[100%] h-[100%] bg-opacity-25 bg-[#000000]"></div>
       </div>
     </>
--- frontend/src/pages/homepage/productsSection/featuredCategories.js
+++ frontend/src/pages/homepage/productsSection/featuredCategories.js
@@ -1,71 +1,38 @@
 import React from "react";
 import { BsArrowRight } from "react-icons/bs";
+import bedroomCategoryBgImg from "../../../assets/bedRoomCategory.jpg";
+import kidsCategoryBgImg from "../../../assets/kidsCategory.jpg";
+import firstOrderCategoryBgImg from "../../../assets/firstOrderCategory.jpg";
+import livingRoomCategoryBgImg from "../../../assets/livingRoomCategory.jpg";
 
 export const FeaturedCategories = () => {
+  const categoriesFaceArr = [
+    { title: "kids", src: kidsCategoryBgImg },
+    { title: "first order deal", src: firstOrderCategoryBgImg },
+    { title: "bedroom", src: bedroomCategoryBgImg },
+    { title: "living room", src: livingRoomCategoryBgImg },
+  ];
   return (
     <section className="my-16">
       <h2 className="text-[36px] text-center mb-10 font-bold mx-4">Featured Categories</h2>
       <div className="flex flex-col items-center gap-10 md:flex-row md:w-[92%] lg:w-[96%] xl:w-[92%] md:mx-auto md:justify-between md:flex-wrap md:gap-2">
-        <article className="w-[92%] tablet:w-[88%] md:mx-0 md:w-[100%] mx-auto cursor-pointer basis-[45%]">
-          <div className="w-[100%]  relative category-img-container h-[350px] tablet:h-[400px]">
-            <img
-              className="w-[100%] h-[100%] object-cover"
-              src="https://images.pexels.com/photos/2227832/pexels-photo-2227832.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt=""
-            />
-            <div className="product-img-overlay hidden absolute top-0 left-0 z-50 bg-[#0000005d] w-[100%] h-[100%]"></div>
-            <button className="absolute left-[25%] top-[40%] bg-primaryColor text-white hidden cursor-pointer rounded-sm h-[44px] w-[50%] gap-3 justify-center z-[100]  items-center category-shop-link">
-              <span> Shop Now</span>
-              <BsArrowRight />
-            </button>
-          </div>
-          <h2 className="text-[24px] mt-3 font-bold"> First order deal</h2>
-        </article>
-        <article className="w-[92%] md:mx-0 md:w-[100%] tablet:w-[88%] mx-auto cursor-pointer basis-[45%]">
-          <div className="w-[100%]  relative category-img-container h-[350px] tablet:h-[400px]">
-            <img
-              className="w-[100%] h-[100%] object-cover"
-              src="https://images.pexels.com/photos/2227832/pexels-photo-2227832.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt=""
-            />
-            <div className="product-img-overlay hidden absolute top-0 left-0 z-50 bg-[#0000005d] w-[100%] h-[100%]"></div>
-            <button className="absolute left-[25%] top-[40%] bg-primaryColor text-white hidden cursor-pointer rounded-sm h-[44px] w-[50%] gap-3 justify-center z-[100]  items-center category-shop-link">
-              <span> Shop Now</span>
-              <BsArrowRight />
-            </button>
-          </div>
-          <h2 className="text-[24px] mt-3 font-bold">Living room</h2>
-        </article>
-        <article className="w-[92%] md:mx-0 md:w-[100%] tablet:w-[88%] mx-auto cursor-pointer basis-[45%]">
-          <div className="w-[100%]  relative category-img-container h-[350px] tablet:h-[400px]">
-            <img
-              className="w-[100%] h-[100%] object-cover"
-              src="https://images.pexels.com/photos/2227832/pexels-photo-2227832.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt=""
-            />
-            <div className="product-img-overlay hidden absolute top-0 left-0 z-50 bg-[#0000005d] w-[100%] h-[100%]"></div>
-            <button className="absolute left-[25%] top-[40%] bg-primaryColor text-white hidden cursor-pointer rounded-sm h-[44px] w-[50%] gap-3 justify-center z-[100]  items-center category-shop-link">
-              <span> Shop Now</span>
-              <BsArrowRight />
-            </button>
-          </div>
-          <h2 className="text-[24px] mt-3 font-bold">Bedroom</h2>
-        </article>
-        <article className="w-[92%] md:mx-0 md:w-[100%]  tablet:w-[88%] mx-auto cursor-pointer basis-[45%]">
-          <div className="w-[100%]  relative category-img-container h-[350px] tablet:h-[400px]">
-            <img
-              className="w-[100%] h-[100%] object-cover"
-              src="https://images.pexels.com/photos/2227832/pexels-photo-2227832.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt=""
-            />
-            <div className="product-img-overlay hidden absolute top-0 left-0 z-50 bg-[#0000005d] w-[100%] h-[100%]"></div>
-            <button className="absolute left-[25%] top-[40%] bg-primaryColor text-white hidden cursor-pointer rounded-sm h-[44px] w-[50%] gap-3 justify-center z-[100]  items-center category-shop-link">
-              <span> Shop Now</span>
-              <BsArrowRight />
-            </button>
-          </div>
-          <h2 className="text-[24px] mt-3 font-bold"> Kids</h2>
-        </article>
+        {categoriesFaceArr.map((category) => {
+          return (
+            <article className="w-[92%] tablet:w-[88%] md:mx-0 md:w-[100%] mx-auto cursor-pointer basis-[45%]">
+              <div
+                style={{ backgroundImage: `url(${category.src})` }}
+                className="w-[100%]  bg-cover bg-no-repeat bg-center relative category-img-container h-[350px] tablet:h-[400px]"
+              >
+                <div className="product-img-overlay hidden absolute top-0 left-0 z-50 bg-[#0000005d] w-[100%] h-[100%]"></div>
+                <button className="absolute left-[25%] top-[40%] bg-primaryColor text-white hidden cursor-pointer rounded-sm h-[44px] w-[50%] gap-3 justify-center z-[100]  items-center category-shop-link">
+                  <span> Shop Now</span>
+                  <BsArrowRight />
+                </button>
+              </div>
+              <h2 className="text-[24px] mt-3 font-bold capitalize">{category.title}</h2>
+            </article>
+          );
+        })}
       </div>
       <div className="flex flex-col w-[100%] mt-10 gap-10">
         <article className="bg-[#e5e5e5] text-secondaryColor  h-[150px] flex justify-center items-center px-[4%]">
