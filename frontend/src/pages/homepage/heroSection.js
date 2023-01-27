506ed750afeb5ab23f19a9a055208ace1e45ea89
Auf <azeezumarfaruk@gmail.com>
2023-01-27 17:17:27 +0100

    [style] : styled the heroSection and whyChoseUsSevtion more

--- frontend/src/pages/homepage/heroSection.js
+++ frontend/src/pages/homepage/heroSection.js
@@ -1,15 +1,30 @@
 import React from "react";
 import heroImg from "../../assets/heroBg.jpg";
+import { useNavigate } from "react-router-dom";
 
 export const HeroSection = () => {
-  console.log(heroImg);
+  const navigate = useNavigate();
   return (
     <>
       <div
         style={{ backgroundImage: `url(${heroImg})` }}
-        className="w-[100%] h-[300px] tablet:h-[400px] md:h-[452px] lg:h-[560px] xl:h-[652px] 2xl:h-[800px]  bg-cover bg-no-repeat bg-center top-0 relative"
+        className="w-[100%] h-[300px] tablet:h-[400px] md:h-[452px] lg:h-[560px] xl:h-[652px] 2xl:h-[800px]  bg-cover bg-no-repeat bg-center top-0 relative flex justify-center items-center"
       >
-        <div className="overlay absolute top-0 left-0 right-0 w-[100%] h-[100%] bg-opacity-25 bg-[#000000]"></div>
+        <div className="overlay absolute top-0 left-0 right-0 w-[100%] h-[100%] bg-opacity-50 bg-[#000000]"></div>
+        <div className="justify-center items-center w-[90%] mx-auto flex max-w-[500px] relative tablet  flex-col gap-3 tablet:gap-4 h-[100%] text-white md:gap-5 ">
+          <h2 className="text-[32px] tablet:text-[36px] md:text-[44px] lg:text-[52px] xl:text-[56px] font-bold text-center">
+            Design Your Spaces, <br /> Your Way
+          </h2>
+          <p className="text-center lg:text-lg ">
+            Upgrade your home and offices with our curated selection of furniture and decor
+          </p>
+          <button
+            className=" lg:w-[11.1rem] lg:h-[72px] w-[9.4rem] h-[52px] md:h-[56px] font-medium  border-[3px] hover:text-white hover:bg-primaryColor hover:border-transparent hover:font-normal duration-500 ease-in-out border-primaryColor text-primaryColor px-2"
+            onClick={() => navigate("/shop")}
+          >
+            Shop now
+          </button>
+        </div>
       </div>
     </>
   );
--- frontend/src/pages/homepage/whyChooseUsSection.js
+++ frontend/src/pages/homepage/whyChooseUsSection.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { FaTruck } from "react-icons/fa";
+import { AiOutlineSafety } from "react-icons/ai";
+import { BiUserVoice } from "react-icons/bi";
+import { TbDiscount2 } from "react-icons/tb";
 
 export const WhyChooseUsSection = () => {
   return (
@@ -11,40 +14,46 @@ export const WhyChooseUsSection = () => {
             <FaTruck className="fill-secondaryColor w-9 h-9 md:w-12 md:h-12 tablet:w-12 tablet:h-12 " />
           </div>
           <div className="flex flex-col  gap-4">
-            <h4 className="text-[24px]  text-center font-bold font-RobotoCondensed">Free delivery</h4>
-            <p className="text-center leading-[140%] text-to-be-wrapped">Lorem ipsum dolor sit amet.nnannanaannananp</p>
+            <h4 className="text-[24px]  text-center font-bold font-RobotoCondensed">Free Shipping</h4>
+            <p className="text-center leading-[140%] text-to-be-wrapped">
+              We offer free shipping on all orders, so you can shop with confidence knowing that there are no hidden
+              costs."
+            </p>
           </div>
         </div>
         <div className="flex w-[100%] md:basis-[45%] flex-row items-center gap-4  tablet:gap-6 tablet:w-[90%] tablet:max-w-[384px]">
           <div className="bg-primaryColor  p-8 rounded-[50%] mx-auto">
-            <FaTruck className="fill-secondaryColor w-9 h-9 md:w-12 md:h-12 tablet:w-12 tablet:h-12  " />
+            <BiUserVoice className="fill-secondaryColor w-9 h-9 md:w-12 md:h-12 tablet:w-12 tablet:h-12  " />
           </div>
           <div className="flex flex-col gap-4">
-            <h4 className="text-[24px]  text-center font-bold  font-RobotoCondensed">Free delivery</h4>
+            <h4 className="text-[24px]  text-center font-bold  font-RobotoCondensed">Customer Service</h4>
             <span className="text-center leading-[140%] text-to-be-wrapped">
-              Lorem ipsum dolor sit amet.nnannanaannanan
+              Our customer service team is available to help you with any issues or concerns. We're here to make sure
+              you are completely satisfied with your purchase
             </span>
           </div>
         </div>
         <div className="flex w-[100%] md:basis-[45%] flex-row items-center gap-4 tablet:gap-6 tablet:w-[90%] tablet:max-w-[384px]">
           <div className="bg-primaryColor  p-8 rounded-[50%] mx-auto">
-            <FaTruck className="fill-secondaryColor w-9 h-9 md:w-12 md:h-12 tablet:w-12 tablet:h-12  " />
+            <TbDiscount2 className="fill-secondaryColor w-9 h-9 md:w-12 md:h-12 tablet:w-12 tablet:h-12  " />
           </div>
           <div className="flex flex-col gap-4">
-            <h4 className="text-[24px]  text-center font-bold font-RobotoCondensed">Free delivery</h4>
+            <h4 className="text-[24px]  text-center font-bold font-RobotoCondensed">Exclusive Offers and Discounts</h4>
             <span className="text-center leading-[140%] text-to-be-wrapped">
-              Lorem ipsum dolor sit amet.nnannanaannanan
+              We're constantly updating our inventory with exclusive offers and products that you won't find anywhere
+              else. From limited-edition items to one-of-a-kind pieces, we have something for everyone
             </span>
           </div>
         </div>
         <div className="flex w-[100%] md:basis-[45%]  items-center gap-4 tablet:gap-6 tablet:w-[90%] tablet:max-w-[384px]">
           <div className="bg-primaryColor  p-8 rounded-[50%] mx-auto">
-            <FaTruck className="fill-secondaryColor w-9 h-9 md:w-12 md:h-12 tablet:w-12 tablet:h-12  " />
+            <AiOutlineSafety className="fill-secondaryColor w-9 h-9 md:w-12 md:h-12 tablet:w-12 tablet:h-12  " />
           </div>
           <div className="flex flex-col gap-4">
-            <h4 className="text-[24px]  text-center font-bold font-RobotoCondensed">free delivery</h4>
+            <h4 className="text-[24px]  text-center font-bold font-RobotoCondensed">Secure and safe payment</h4>
             <span className="text-center leading-[140%] text-to-be-wrapped">
-              Lorem ipsum dolor sit amet.nnannanaannanan
+              We offer a range of safe payment options, so you can shop with confidence knowing that your information is
+              protected every step of the way."
             </span>
           </div>
         </div>
