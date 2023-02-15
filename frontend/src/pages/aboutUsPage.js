e54c1df922922f9f2c223be75f102ee40dabcf77
Auf <azeezumarfaruk@gmail.com>
2023-02-15 23:36:54 +0100

    [feat] : Added initial design of about us and contact us page

--- frontend/src/pages/aboutUsPage.js
+++ frontend/src/pages/aboutUsPage.js
@@ -1,3 +1,48 @@
+import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+import FooterSection from "../components/footerSection";
+import { WhyChooseUsSection } from "./homepage/whyChooseUsSection";
+import FurnitureVector from "../assets/Home-furniture-set.jpg";
+
 export const AboutUsPage = () => {
-  return <h1>About us</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="mt-12 w-[100%] h-[54px] bg-neutralColor text-secondaryColor tablet:px-[6%] xl:px-[4%] px-[4%] lg:px-[2%]  flex items-center justify-between font-bold  font-RobotoCondensed lg:col-span-full lg:row-span-1">
+        <div className="flex gap-[4px] items-center text-[15px]">
+          <IoIosArrowBack />
+          <li onClick={() => navigate("/")} className="hover:underline capitalize">
+            Home
+          </li>
+          <IoIosArrowBack />
+          <li onClick={() => navigate("/shop")} className="hover:underline capitalize">
+            Shop
+          </li>
+          <IoIosArrowBack />
+          <span className=" capitalize">About</span>
+        </div>
+      </div>
+      <section className="w-full  mt-4 tablet:px-[6%] xl:px-[4%] px-[4%] lg:px-[2%] flex flex-col md:flex-row gap-4  pt-20 pb-10">
+        <div>
+          <h2 className="text-[28px] md:text-[32px] lg:text-[36px] mb-2">Our Story</h2>
+          <p className="leading-[180%] first-letter:float-left first-letter:mr-4 first-letter:text-7xl  first-letter:text-secondaryColor first-letter:font-bold">
+            Welcome to our e-furniture store,Auffur, where we offer a wide selection of high-quality, affordable
+            furniture for every room in your home. Our online platform allows you to shop for furniture at your
+            convenience, with easy-to-use filters that help you find the perfect piece for your style and budget. We
+            believe that everyone deserves to have a beautiful, comfortable home, and we're committed to making that a
+            reality for our customers. Our furniture is designed and crafted by experts using only the best components,
+            ensuring that you get furniture that is both stylish and durable. At Auffur, we're passionate about creating
+            beautiful spaces and providing excellent customer service, and we look forward to helping you create your
+            dream home.
+          </p>
+        </div>
+        <div className=" flex justify-center items-center">
+          <img className="" src={FurnitureVector} alt="" />
+        </div>
+      </section>
+      <WhyChooseUsSection />
+      <FooterSection />
+    </>
+  );
 };
--- frontend/src/pages/contactUsPage.js
+++ frontend/src/pages/contactUsPage.js
@@ -1,3 +1,75 @@
+import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+import FooterSection from "../components/footerSection";
+
 export const ContactUsPage = () => {
-  return <h1>Contact us</h1>;
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="mt-12 w-[100%] h-[54px] bg-neutralColor text-secondaryColor tablet:px-[6%] xl:px-[4%] px-[4%] lg:px-[2%]  flex items-center justify-between font-bold  font-RobotoCondensed lg:col-span-full lg:row-span-1">
+        <div className="flex gap-[4px] items-center text-[15px]">
+          <IoIosArrowBack />
+          <li onClick={() => navigate("/")} className="hover:underline capitalize">
+            Home
+          </li>
+          <IoIosArrowBack />
+          <li onClick={() => navigate("/shop")} className="hover:underline capitalize">
+            Shop
+          </li>
+          <IoIosArrowBack />
+          <span className=" capitalize">Contact us</span>
+        </div>
+      </div>
+
+      <section className="w-[100%] mt-20 mb-28 tablet:px-[6%] xl:px-[4%] px-[4%] lg:px-[2%] flex justify-center items-center ">
+        <form action="" className="w-[92%] max-w-[360px] flex flex-col gap-4 " onSubmit={handleSubmit}>
+          <h2 className="text-[36px] text-center mb-8 lg:text-[44px]">Contact Us</h2>
+          <div>
+            <label htmlFor="">
+              Name
+              <input
+                type="text"
+                name=""
+                className="border pl-4 focus:outline-none rounded w-full mt-2 h-[52px]"
+                id=""
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="">
+              Email
+              <input
+                type="text"
+                name=""
+                className="border pl-4 focus:outline-none rounded w-full mt-2 h-[52px]"
+                id=""
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="">Message</label>
+            <textarea
+              className="border p-4 focus:outline-none mt-2 border-LightSecondaryColor rounded w-full"
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="my-4 w-[100%] mx-auto block h-[52px] bg-primaryColor text-white cursor-none font-medium rounded"
+          >
+            Send message
+          </button>
+        </form>
+      </section>
+      <FooterSection />
+    </>
+  );
 };
